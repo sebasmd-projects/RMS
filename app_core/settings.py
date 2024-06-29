9c82af3ad1c40ee8de02f8aeacba51eea986cd80
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-06-29 02:59:10 -0500

    remove rms static

--- app_core/settings.py
+++ app_core/settings.py
@@ -166,7 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = str(BASE_DIR / 'public' / 'staticfiles')
 
-STATIC_URL = 'rms/public/static/'
+STATIC_URL = 'public/static/'
 
 STATICFILES_DIRS = [str(BASE_DIR / 'public' / 'static')]
 
