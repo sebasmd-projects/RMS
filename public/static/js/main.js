7fb30a768230f16b10d321e787a23b2745d33805
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-06-30 21:38:34 -0500

    test

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -6,8 +6,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.querySelector('.contact-form');
     var submitButton = document.getElementById('contact-submit-button');
-  
+    console.log("holi")
     form.addEventListener('submit', function () {
+      console.log("holi")
       submitButton.disabled = true;
       submitButton.textContent = '...';
     });
